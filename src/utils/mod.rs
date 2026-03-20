5654250bb5997772ce024ef9da7c933dd452cede
SteampunkIslande <charles.monod-broca@neuf.fr>
2026-03-21 00:59:20 +0100

    Big refactoring

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,8 +4,7 @@
 //! including column definitions, color maps, and data transformation functions.
 
 pub mod utils;
+pub use utils::*;
 
-pub use utils::{
-    ALL_COLUMNS, ColorMapEntry, INTERESTING_COLUMNS, USEFUL_COLUMNS, add_daily_duration,
-    add_job_duration_cols, add_wait_time_cols, get_color_map, get_default_cmap,
-};
+pub mod lazyframe_edit;
+pub use lazyframe_edit::*;
