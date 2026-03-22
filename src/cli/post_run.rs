e81a4e1a1711a07b650ab507feda3650366e2716
SteampunkIslande <charles.monod-broca@neuf.fr>
2026-03-22 15:33:36 +0100

    WIP

--- src/cli/post_run.rs
+++ src/cli/post_run.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use std::path::PathBuf;
+use slurm_usage_report_rs::{sacct_get, snakemake_parse_log, utils};
+use std::path::{Path, PathBuf};
 
 const LONG_ABOUT: &'static str = concat!(
     "Script post-run pour générer un rapport d'usage Slurm à partir des logs de Snakemake.\n\n",
@@ -34,11 +35,26 @@ pub struct PostRunCmd {
 
 impl PostRunCmd {
     pub fn run(&self) {
+        let slurm_job_names = snakemake_parse_log::get_slurm_ids(
+            &self.input.iter().map(|p| p.as_path()).collect::<Vec<_>>(),
+        )
+        .expect("Error reading log files:");
+
         let parquets_of_interest: Vec<PathBuf> = match &self.db {
             Some(db) => {
                 todo!()
             }
             None => {
+                sacct_get::get_sacct_for_runs(
+                    &slurm_job_names
+                        .iter()
+                        .map(|s| s.as_str())
+                        .collect::<Vec<_>>(),
+                    Path::new("sacct.csv"),
+                )
+                .expect("Error trying to get sacct info");
+                utils::sacct_sanitizer(&Path::new("sacct.csv"), None, None)
+                    .expect("Error trying to sanitize sacct CSV");
                 todo!()
             }
         };
--- src/post_run/mod.rs
+++ src/post_run/mod.rs
@@ -3,3 +3,6 @@ pub use snakemake_rules_plot::*;
 
 pub mod snakemake_parse_log;
 pub use snakemake_parse_log::*;
+
+pub mod sacct_get;
+pub use sacct_get::*;
